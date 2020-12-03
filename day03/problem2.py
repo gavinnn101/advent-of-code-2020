082edded8d200b10787a6699f77cea8333dd1896
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2020-12-03 18:01:46 -0500

    Update problem2.py
    working solution to day 3, problem 2

--- day03/problem2.py
+++ day03/problem2.py
@@ -9,26 +9,28 @@ def traverse(graph: list, move_right: int, move_down: int) -> int:
     trees = 0
     landing_spot = None
 
-    for line in graph:
-        line *= len(graph)  # Repeat the graph per question requirements
-        for element in range(position, len(line[:position+move_right+1])):
+    for line in range(0, len(graph), move_down):  # From the start of the list to the end, skipping `move_down` lines
+        print(f"Line Number: {line}")
+        graph[line] *= len(graph)  # Extend the line to the right per question requirements
+        for element in range(position, len(graph[line][:position+move_right+1])):
             if element == landing_spot:
-                if line[element] == '#':
+                if graph[line][element] == '#':
                     trees += 1
-                    print(f"Hit a tree at {element}")
-            print(element, line[element])
+            print(element, graph[line][element])
             position += 1
-        position -= move_down
+        position -= 1  # Start at the space directly below on our next line
         landing_spot = position
         print('---------')
+        print(f"Landing spot: {landing_spot} {graph[line][landing_spot]}")
 
     return trees
 
-
-# a = (traverse(graph, move_right=1, move_down=1))
-# b = (traverse(graph, move_right=3, move_down=1))
-# c = (traverse(graph, move_right=5, move_down=1))
-# d = (traverse(graph, move_right=7, move_down=1))
+# Test inputs
+# Trying to store all of this in memory causes a MemoryError but I'm moving on..
+a = (traverse(graph, move_right=1, move_down=1))
+b = (traverse(graph, move_right=3, move_down=1))
+c = (traverse(graph, move_right=5, move_down=1))
+d = (traverse(graph, move_right=7, move_down=1))
 e = (traverse(graph, move_right=1, move_down=2))
 
-# print(a * b * c * d * e)
+print(a * b * c * d * e)
